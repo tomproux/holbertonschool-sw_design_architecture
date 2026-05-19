a815edebdc6511b00373bc205ea7c1cc6ac71d32
tomproux <tomproux53@gmail.com>
2026-05-19 09:55:07 +0200

    ok

--- design_patterns/1-observer.py
+++ design_patterns/1-observer.py
@@ -11,7 +11,8 @@ class NewsSubject:
     def __init__(self) -> None:
         self._subs: dict[Observer, set[str] | None] = {}
 
-    def subscribe(self, observer: Observer, topics: set[str] | None = None) -> None:
+    def subscribe(self, observer: Observer,
+                  topics: set[str] | None = None) -> None:
         if observer in self._subs:
             return  # ignore duplicate subscribe for same instance
         self._subs[observer] = topics
@@ -52,7 +53,7 @@ def main() -> None:
     subject.subscribe(log, topics={"sports", "breaking"})
     subject.subscribe(email)  # None = receives all topics
 
-    # TODO: instantiate SmsObserver and subscribe it to topics={"breaking"} only
+    # TODO:instantiate SmsObserver and subscribe it to topics={"breaking"}only
     sms = SmsObserver()
 
     subject.subscribe(sms, topics={"breaking"})
